26851eb0fb86f5615e0eb7c5c4c3e15082c75400
mconnolly <mconnolly@blinfo.com>
2023-02-17 15:03:17 -0500

    add finished test deck

--- poker.js
+++ poker.js
@@ -43,14 +43,124 @@ class Deck {
 	}
 }
 
-class TestDeck extends Deck {
-	dealStraight() {
+class TestDeck {
+	dealRoyalFlush() {
 		const hand = new Hand;
 		hand.addCard(new Card('10','Hearts'));
 		hand.addCard(new Card('J','Hearts'));
+		hand.addCard(new Card('Q','Hearts'));
+		hand.addCard(new Card('K','Hearts'));
+		hand.addCard(new Card('A','Hearts'));
+		
+		return hand;
+	}
+
+	dealStraightFlush() {
+		const hand = new Hand;
+		hand.addCard(new Card('9','Diamonds'));
+		hand.addCard(new Card('10','Diamonds'));
+		hand.addCard(new Card('J','Diamonds'));
 		hand.addCard(new Card('Q','Diamonds'));
-		hand.addCard(new Card('K','Spades'));
-		hand.addCard(new Card('A','Clubs'));
+		hand.addCard(new Card('K','Diamonds'));
+		
+		return hand;
+	}
+
+	dealFourOfAKind() {
+		const hand = new Hand;
+		hand.addCard(new Card('5','Clubs'));
+		hand.addCard(new Card('5','Spades'));
+		hand.addCard(new Card('5','Clubs'));
+		hand.addCard(new Card('5','Diamonds'));
+		hand.addCard(new Card('7','Clubs'));
+		
+		return hand;
+	}
+
+	dealFullHouse() {
+		const hand = new Hand;
+		hand.addCard(new Card('8','Clubs'));
+		hand.addCard(new Card('2','Spades'));
+		hand.addCard(new Card('8','Clubs'));
+		hand.addCard(new Card('2','Diamonds'));
+		hand.addCard(new Card('8','Clubs'));
+		
+		return hand;
+	}
+
+	dealFlush() {
+		const hand = new Hand;
+		hand.addCard(new Card('10','Hearts'));
+		hand.addCard(new Card('4','Hearts'));
+		hand.addCard(new Card('Q','Hearts'));
+		hand.addCard(new Card('K','Hearts'));
+		hand.addCard(new Card('7','Hearts'));
+		
+		return hand;
+	}
+
+	dealStraight() {
+		const hand = new Hand;
+		hand.addCard(new Card('7','Spades'));
+		hand.addCard(new Card('8','Hearts'));
+		hand.addCard(new Card('9','Diamonds'));
+		hand.addCard(new Card('10','Clubs'));
+		hand.addCard(new Card('J','Diamonds'));
+		
+		return hand;
+	}
+
+	dealWheel() {
+		const hand = new Hand;
+		hand.addCard(new Card('A','Spades'));
+		hand.addCard(new Card('3','Hearts'));
+		hand.addCard(new Card('2','Diamonds'));
+		hand.addCard(new Card('5','Clubs'));
+		hand.addCard(new Card('4','Diamonds'));
+		
+		return hand;
+	}
+
+	dealThreeOfAKind() {
+		const hand = new Hand;
+		hand.addCard(new Card('4','Clubs'));
+		hand.addCard(new Card('9','Spades'));
+		hand.addCard(new Card('4','Clubs'));
+		hand.addCard(new Card('4','Diamonds'));
+		hand.addCard(new Card('7','Clubs'));
+		
+		return hand;
+	}
+
+	dealTwoPair() {
+		const hand = new Hand;
+		hand.addCard(new Card('2','Clubs'));
+		hand.addCard(new Card('2','Spades'));
+		hand.addCard(new Card('7','Clubs'));
+		hand.addCard(new Card('7','Diamonds'));
+		hand.addCard(new Card('10','Clubs'));
+		
+		return hand;
+	}
+
+	dealPair() {
+		const hand = new Hand;
+		hand.addCard(new Card('2','Clubs'));
+		hand.addCard(new Card('2','Spades'));
+		hand.addCard(new Card('8','Clubs'));
+		hand.addCard(new Card('Q','Diamonds'));
+		hand.addCard(new Card('10','Clubs'));
+		
+		return hand;
+	}
+
+	dealHighCard() {
+		const hand = new Hand;
+		hand.addCard(new Card('K','Clubs'));
+		hand.addCard(new Card('2','Spades'));
+		hand.addCard(new Card('7','Clubs'));
+		hand.addCard(new Card('9','Diamonds'));
+		hand.addCard(new Card('4','Clubs'));
 		
 		return hand;
 	}
@@ -80,13 +190,40 @@ class Hand {
 		this.ranks.sort((a, b) => a - b);
 	}
 
+	isFlush() {
+		for (const suit in this.suits) {
+			if (this.suits[suit] === 5)
+				return true;
+		}
+		return false;
+	}
+
 	isStraight() {
-		for (let i = 0; i < this.ranks.length; i++) {
+		for (let i = 0; i < this.ranks.length - 1; i++) {
 			if (this.ranks[i + 1] - this.ranks[i] !== 1) {
 				return false;
 			}
 		}
 		return true;
+	}
+
+	checkMatches(num) {
+		// list of cards
+		let matches = {};
+
+		for (const rank of this.ranks) {
+			matches[rank] = matches[rank] + 1 || 1;
+		}
+
+		for (const match in matches) {
+			if (matches[match] === num) {
+				return true;
+			}
+		}
+	}
+
+	checkPairs() {
+
 	}
 }
 
@@ -117,29 +254,10 @@ class Card {
 	}
 }
 
-let deck = new Deck;
-deck.shuffleDeck();
+let deck = new TestDeck;
 
-let hand = new Hand;
-hand.addCard(deck.dealCard());
-hand.addCard(deck.dealCard());
-hand.addCard(deck.dealCard());
-hand.addCard(deck.dealCard());
-hand.addCard(deck.dealCard());
+let hand = deck.dealFlush();
 
 // test hand
-console.log('HAND:')
 console.log(hand.cards)
-console.log('RANKS:')
-console.log(hand.ranks);
-console.log('SUITS:')
-console.log(hand.suits);
-console.log('Straight?')
-console.log(hand.isStraight())
-
-console.log(`\\\\\\\\\\\\\\\\\\\\\\\\`)
-let testDeck = new TestDeck;
-let handStraight = testDeck.dealStraight();
-console.log(handStraight.cards);
-console.log(handStraight.ranks);
-console.log(`IS STRAIGHT: ${handStraight.isStraight()}`)
+console.log(hand.checkMatches())
